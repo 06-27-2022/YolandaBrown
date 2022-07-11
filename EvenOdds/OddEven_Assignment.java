7f396acbb900601cc5cbae907659073a34a80a27
ybrown30 <y_brown18@yahoo.com>
2022-07-11 03:25:51 -0400

    assignment

---
+++ EvenOdds/OddEven_Assignment.java
@@ -0,0 +1,29 @@
+package EvenOdds;
+import java.util.Scanner;
+
+
+public class OddEven_Assignment {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner fName1 = new Scanner(System.in);
+		System.out.println("Hi, please enter your name");
+	    String name= fName1.nextLine();			
+		System.out.println(" Hi " + name);
+		
+	 
+	
+		Scanner reader1 = new Scanner(System.in);			
+		
+		System.out.print("Enter a number and I will tell you if it is odd or even ");
+        int userInput = reader1.nextInt();
+        
+        if(userInput % 2 == 0)
+            System.out.println("The number you entered " + userInput + " is an even number" );
+        else
+            System.out.println("The number you entered " + userInput + " is an odd number" );
+
+}
+	
+}//end of class
---
+++ EvenOrOdds.java
@@ -0,0 +1,9 @@
+
+public class EvenOrOdds {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
