69ab39aaa993b72662f16692cf035a037b56ff17
ybrown30 <y_brown18@yahoo.com>
2022-07-11 03:42:23 -0400

    close scanner

--- EvenOdds/OddEven_Assignment.java
+++ EvenOdds/OddEven_Assignment.java
@@ -15,15 +15,17 @@ public class OddEven_Assignment {
 	 
 	
 		Scanner reader1 = new Scanner(System.in);			
-		
+		  reader1.close();  
 		System.out.print("Enter a number and I will tell you if it is odd or even ");
         int userInput = reader1.nextInt();
         
+       
         if(userInput % 2 == 0)
             System.out.println("The number you entered " + userInput + " is an even number" );
         else
             System.out.println("The number you entered " + userInput + " is an odd number" );
 
-}
-	
+ 
+        
+	}	
 }//end of class
